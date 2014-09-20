86841977285b2a68e95915c44d323a47f364e1d5
Brian Lloyd-Newberry <newbeb@gmail.com>
2014-09-20 10:34:53 -0400

    Initial pixel server

---
+++ pixel/src/main/java/bln/pixal/PixelListener.java
@@ -0,0 +1,41 @@
+package bln.pixal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * A simple Pixel listener
+ * Created by ddcbrianln on 9/18/14.
+ */
+@Controller
+@EnableAutoConfiguration
+public class PixelListener {
+
+    private Logger log = LoggerFactory.getLogger("PixalLog");
+
+    @RequestMapping("/")
+    @ResponseBody
+    String ping(HttpServletRequest request, HttpServletResponse response)
+    {
+
+        final String referer = request.getHeader("referer");
+
+        log.info("Handled request for: {} by {}", request.getServerName(), referer);
+
+        return "Hello " + referer;
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(PixelListener.class, args);
+    }
+}
