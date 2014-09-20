2affafc0939349db9903174adb1e3fa2ec974b20
Brian Lloyd-Newberry <newbeb@gmail.com>
2014-09-20 10:31:57 -0400

    Dockerfile for centos based JVM

---
+++ centos-jvm/pulljava.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Pull the latest oracle JVM
+curl -L -C - -b "oraclelicense=accept-securebackup-cookie" -O http://download.oracle.com/otn-pub/java/jdk/8u11-b12/jdk-8u11-linux-x64.tar.gz -o jdk8-linux-x64.tar.gz
+Qwq
+Qwq
+#!/bin/sh
+
+# Pull the latest oracle JVM
+curl -L -C - -b "oraclelicense=accept-securebackup-cookie" -O http://download.oracle.com/otn-pub/java/jdk/8u11-b12/jdk-8u11-linux-x64.tar.gz -o jdk8-linux-x64.tar.gz
